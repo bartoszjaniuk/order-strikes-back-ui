4a77c3d99bd46be2756d7cb9f732268534734ddc
Bartosz <bartosz.janiuk@tsh.io>
2024-07-06 14:11:05 +0200

    fix

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,6 +1,5 @@
 export type Direction = "row" | "column" | "column-reverse" | "row-reverse";
 export type ResponsiveDirection = Direction | [Direction, Direction];
-import "../../styles/style.css";
 
 type TailwindDirections =
 	| "flex-row"
