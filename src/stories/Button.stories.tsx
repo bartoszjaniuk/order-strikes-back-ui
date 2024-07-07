3b98716b224f82fbab8a15863ac02246d992524e
Bartosz <bartosz.janiuk@tsh.io>
2024-07-07 10:04:51 +0200

    import fix

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import React from "react";
-import { Button } from "../components/Button";
+import { Button } from "src/components/Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
